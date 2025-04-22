0fd76ef191aa749de612c319d63a7fc19990544b
Saman Mohammed Sherif <104517358+MOHAMMEDsaman123@users.noreply.github.com>
2025-04-22 15:43:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utils import mask_pii, build_response
+from models import load_model, predict_category
+
+app = FastAPI()
+
+model, encoder = load_model()
+
+class EmailRequest(BaseModel):
+    email: str
+
+@app.post("/classify_email")
+def classify_email(request: EmailRequest):
+    original_email = request.email
+    masked_email, entities = mask_pii(original_email)
+    category = predict_category(masked_email, model, encoder)
+    return build_response(original_email, masked_email, entities, category)
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI is working! Visit /docs"}
